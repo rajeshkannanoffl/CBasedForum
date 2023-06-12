bebf72fe7a4fbccbaca5c13098721d47bd84b8b7
Rajesh Kannan M <rajeshkannan.offl@gmail.com>
2023-06-12 12:11:25 +0530

    Personal Update

---
+++ cppworks/swapptr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void SwapInt(int *a, int *b){
+	int *temp;
+	cout << "Before Swapping: "<<endl;
+	cout << "\tA = " << *a << ": B = " << *b << endl;
+	cout << "After Swapping: "<<endl;
+	temp = a;
+	a = b; 
+	b = temp;
+	cout << "\tA = " << *a << ": B = " << *b << endl;
+}
+
+int main(){
+	int a, b, *pa = &a, *pb = &b;
+	cout << "Enter A: ";
+	cin >> a;
+	cout << "Enter B: ";
+        cin >> b;
+	SwapInt(pa,pb);
+	return 0;
+}
